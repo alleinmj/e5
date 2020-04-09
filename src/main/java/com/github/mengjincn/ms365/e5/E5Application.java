646b752ff4173ee53a540b0db3c4f6488d6e1573
mengjin <mengjin@sightp.com>
2020-04-09 14:41:05 +0800

    init E5 project

---
+++ src/main/java/com/github/mengjincn/ms365/e5/E5Application.java
@@ -0,0 +1,13 @@
+package com.github.mengjincn.ms365.e5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mengjincn/ms365/e5/E5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mengjincn.ms365.e5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
